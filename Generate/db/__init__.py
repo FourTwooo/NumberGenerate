48b01de73cdb6a7c65ebbc27faee933a74e5d1bb
FourTwo <1666671111@qq.com>
2024-09-19 11:09:01 +0800

    pandas替代为sqlite3,

--- Generate/db/__init__.py
+++ Generate/db/__init__.py
@@ -1,39 +1,90 @@
+import csv
 import os
-
-import pandas as pd
+import sqlite3
 
 current_dir_path = os.path.dirname(os.path.abspath(__file__))
-table = pd.read_csv(f"{current_dir_path}/city_db_data.csv")
-# with open(f"{current_dir_path}/CommonlyUsedSurname.txt", 'r', encoding='utf-8') as file:
-#     CommonlyUsedSurname = list(file.read())
+# 创建SQLite连接
+conn = sqlite3.connect(f"{current_dir_path}/city_db_data.db")
+cur = conn.cursor()
 
+
+def csv_db():
+    global cur
+    # 创建表
+    cur.execute("""
+        CREATE TABLE city_data(
+            省 TEXT,
+            省行政区划代码 TEXT,
+            市 TEXT,
+            市行政区划代码 TEXT,
+            区 TEXT,
+            区行政区划代码 TEXT,
+            来源 TEXT
+        )
+    """)
+
+    # 将CSV数据插入数据库
+    with open(f"{current_dir_path}/city_db_data.csv", 'r', encoding='utf-8') as fin:
+        dr = csv.DictReader(fin)  # 使用csv.DictReader读取CSV文件
+        to_db = [(i['省'], i['省行政区划代码'], i['市'], i['市行政区划代码'], i['区'], i['区行政区划代码'], i['来源'])
+                 for i in dr]
+
+    cur.executemany("""
+        INSERT INTO city_data (省, 省行政区划代码, 市, 市行政区划代码, 区, 区行政区划代码, 来源)
+        VALUES (?, ?, ?, ?, ?, ?, ?);
+    """, to_db)
+    conn.commit()
+
+
+# import pandas as pd
+# table = pd.read_csv(f"{current_dir_path}/city_db_data.csv")
+#
+#
+# def get_area_codes(address: str):
+#     province, city, area = address.split("|")
+#     results = table.copy()
+#     if province:
+#         results = results[results['省'].str.contains(province)]
+#     if city:
+#         results = results[results['市'].str.contains(city)]
+#     if area:
+#         results = results[results['区'].str.contains(area)]
+#
+#     result = []
+#     for index, row in results.iterrows():
+#         source = row['来源']
+#         area = row['区']
+#         area_code = row['区行政区划代码']
+#         city = row['市']
+#         city_code = row['市行政区划代码']
+#         province = row['省']
+#         province_code = row['省行政区划代码']
+#         # print(f'[{area_code}]{province} {city} {area}, 数据来源:{source}')
+#         result.append(area_code)
+#     return result
 
 def get_area_codes(address: str):
     province, city, area = address.split("|")
-    results = table.copy()
-    if province:
-        results = results[results['省'].str.contains(province)]
-    if city:
-        results = results[results['市'].str.contains(city)]
-    if area:
-        results = results[results['区'].str.contains(area)]
+
+    query = f"""
+        SELECT 省, 市, 区, 区行政区划代码, 来源 
+        FROM city_data
+        WHERE 省 LIKE '%{province}%' AND 市 LIKE '%{city}%' AND 区 LIKE '%{area}%'
+    """
+    cur.execute(query)
 
     result = []
-    for index, row in results.iterrows():
-        source = row['来源']
-        area = row['区']
-        area_code = row['区行政区划代码']
-        city = row['市']
-        city_code = row['市行政区划代码']
-        province = row['省']
-        province_code = row['省行政区划代码']
-        # print(f'[{area_code}]{province} {city} {area}, 数据来源:{source}')
-        result.append(area_code)
+    for item in cur.fetchall():
+        # print(item)
+        result.append(item[3])
+
     return result
 
 
 if __name__ == '__main__':
     print(get_area_codes(
-        address='福建|三明|'
+        address='|南通|市辖区'
     ))
-    # print(CommonlyUsedSurname)
+
+    # csv_db()
+
