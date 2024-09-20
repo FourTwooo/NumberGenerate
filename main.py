6dc1741e436994ddfcb4f793a763388139a2a176
FourTwo <1666671111@qq.com>
2024-09-20 16:41:09 +0800

    更新手机号归属地数据库, 新增运营商号段生成

--- main.py
+++ main.py
@@ -1,30 +1,41 @@
 import Generate
 
 if __name__ == '__main__':
-    IDCard = Generate.IDCardGenerate()
-    # IDCard.api_function = lambda address: ["320505"]
-    date_data = IDCard.get_id_card(
-        id_card="320***200205071516",
-        # address="广东|揭阳|",
-        gender="男",
-        # constellation="狮子座",
-        # zodiac="龙"
-    )
-    print(len(date_data), str(date_data)[:200])
+    # IDCard = Generate.IDCardGenerate()
+    # IDCard.START_YEAR = 2000
+    # IDCard.END_YEAR = 2010
+    # # IDCard.api_function = lambda address: ["320505"]
+    # date_data = IDCard.get_id_card(
+    #     id_card="5*****20050319***4",
+    #     # id_card="320***200205071516",
+    #     # address="广东|揭阳|",
+    #     gender="男",
+    #     # constellation="狮子座",
+    #     # zodiac="龙"
+    # )
+    # print(len(date_data), str(date_data)[:200])
 
     Phone = Generate.PhoneGenerate()
+    # Phone.is_db = False
+    from Generate import api
 
+    # print(api.PHONE_ISP_CODES)
+    # Phone.api_function = api.cha_hao_baw
+    # Phone.api_function = api.tel_phone
     phones = Phone.get_phone(
-        city_name="毕节",
-        incomplete_phone="150******34"
+        city_name="北京",
+        incomplete_phone="1*******434",
+        isp="虚拟"
     )
-    print(len(phones), phones)
+    print(len(phones), phones[:20])
 
-    Name = Generate.NameGenerate()
-    names = Name.get_names(['ou', '阳', 'na', '*'])
-    print(len(names), names)
+    # Name = Generate.NameGenerate()
+    # names = Name.get_names(['huang', 'zhi', 'chao'])
+    # print(len(names), names)
+    #
+    # saveFile = Generate.SaveFile()
+    #
+    # saveFile.generate_vcf(phones)
+    # saveFile.generate_txt(date_data)
 
-    saveFile = Generate.SaveFile()
 
-    saveFile.generate_vcf(phones)
-    saveFile.generate_txt(date_data)
